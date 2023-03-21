82c0123d4d691a1ff69e2881ed2da5323c50207f
unknown <asim26k@gmail.com>
2023-03-21 14:17:48 +0530

    configured TailWind CSS

--- LAYOUT_COMPONENTS/modal/src/App.js
+++ LAYOUT_COMPONENTS/modal/src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Hello Worrld !!</h1>
+      <h1 className="text-rose-500">Hello World !!</h1>
     </div>
   );
 }
