968befe9060c4460876b7b5720f6aefdc7d81c93
unknown <asim26k@gmail.com>
2023-03-28 13:46:31 +0530

    Tailwind installed in modal_v1.2

--- LAYOUT_COMPONENTS/modal_v1.2/src/App.jsx
+++ LAYOUT_COMPONENTS/modal_v1.2/src/App.jsx
@@ -1,7 +1,11 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <div className="App">
+      <h1 className=" text-orange-400 ">Hello</h1>
+    </div>
+  );
 }
 
 export default App;
